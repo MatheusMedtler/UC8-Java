b756e878980a7f62da1b26ed53ff48c3f1f6604b
Matheus Medtler Alves <ma284163@gmail.com>
2025-07-02 19:30:09 -0300

    Add files via upload

---
+++ trabalhandocomjlist/TrabalhandoComJlist.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalhandocomjlist;
+
+/**
+ *
+ * @author MATHEUSMEDTLERALVES
+ */
+public class TrabalhandoComJlist {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
